4d4d6d879c0301640b9f290d6b69b804c8398b5e
Manuel <manueldipilla3@gmail.com>
2020-01-16 00:38:04 +0100

    restyling

--- js/script.js
+++ js/script.js
@@ -1,27 +1,80 @@
 var numeriCasuali = [];
-for (var i = 1; i < 17; i++) {
-  var numeroRandom = Math.floor(Math.random()* 100 + 1);
-  numeriCasuali.push(numeroRandom);
-}
-console.log(numeriCasuali);
-var sceltaUtente;
-var numeriIndovinati = 0;
-for (var i = 1; i < 85; i++) {
-  sceltaUtente = parseInt(prompt('inserisci un numero tra 1 e 100'));
-  while (sceltaUtente < 1 || sceltaUtente > 100) {
-    sceltaUtente = parseInt(prompt("numero errato : inserisci un numero da 1 a 100"));
+var numeroUtente = [];
+while (numeriCasuali.length < 16) {
+  var numeroRandom = randomNumber(1, 100);
+  var cerca = isInArray(numeriCasuali, numeroRandom);
+  if(cerca == false) {
+    numeriCasuali.push(numeroRandom);
   }
-  var check = numeriCasuali.includes(sceltaUtente);
+}
+var numMinCheck;
+var numMaxCheck;
+var tentativi;
+var tentativiDifficoltà = 0;
+var numeriInseriti;
 
-  if (check == true) {
-    console.log('hai perso');
-    break;
-  }
-  else if (numeriIndovinati == 84) {
-    console.log('hai vinto');
-  }
-  else {
-    numeriIndovinati++;
+do {
+  var difficoltà = parseInt(prompt('seleziona il livello di difficoltà : da 0 a 2'));
+  tentativiDifficoltà++;
+} while (checkNumber(0, 2 , difficoltà) == false && tentativiDifficoltà < 3);
+
+if (difficoltà == 0) {
+  tentavi = 84;
+  numMinCheck = 1;
+  numMaxCheck = 100;
+} else if (difficoltà == 1) {
+  tentavi = 64;
+  numMinCheck = 1;
+  numMaxCheck = 80;
+} else if (difficoltà == 2) {
+  tentavi = 34;
+  numMinCheck = 1;
+  numMaxCheck = 50;
+}
+
+var bomba = false;
+var numeriIndovinati = 0;
+var messaggio = 'hai vinto';
+while (numeroUtente.length < tentativi && bomba == false) {
+  var numUtente = false;
+  do{
+    var numUtente = parseInt(prompt('inserisci un numero da 1 a 100'));
+    console.log('numero inserito' + checkNumber(numMinChek, numMaxChek, numUtente));
+  } while(checkNumber(numMinChek, numMaxChek, numUtente) == false)
+  if(isInArray(numeroUtente, numUtente) == false) {
+    numeroUtente.push(numUtente);
+    if(isInArray(numeriCasuali, numUtente) == true){
+      messaggio = 'hai perso';
+      bomba = true;
+    } else {
+      numeriIndovinati++;
+    }
   }
 }
+console.log(messaggio);
 console.log('hai indovinato' + ' ' + numeriIndovinati + ' ' + 'numeri');
+
+function randomNumber(numMin, numMax) {
+  numMin = Math.ceil(numMin);
+  numMax = Math.floor(numMax);
+  var result = Math.floor(Math.random() * (numMax - numMin + 1)) + numMin;
+  return  result;
+}
+function checkNumber(min, max, number) {
+  var result = false;
+  if(number >= min && number <= max) {
+    result = true;
+  }
+  return result;
+}
+function isInArray(array, element) {
+  var i = 0;
+  var result = false;
+  while (i < array.length && result == false) {
+    if (array[i] == element) {
+      result = true;
+    }
+    i++;
+  }
+  return result;
+}
